7f2021a02434d78d53e7d2fbdc62c3f5f48d9f34
Qiang <qxl0@yahoo.com>
2022-03-18 20:36:09 -0400

    Added animation based on position

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ import Toast from "./Toast.js";
 document.querySelector("button").addEventListener("click", () => {
   const mytoast = new Toast({
     Text: "Hello World",
-    autoClose: false,
-    canClose: true,
+    position: "top-left",
   });
 });
