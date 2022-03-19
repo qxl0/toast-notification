0e351c0af8d64678eb224e93027e9f6b26d69170
Qiang <qxl0@yahoo.com>
2022-03-18 20:38:27 -0400

    Added support for center

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ import Toast from "./Toast.js";
 document.querySelector("button").addEventListener("click", () => {
   const mytoast = new Toast({
     Text: "Hello World",
-    position: "top-left",
+    position: "top-center",
   });
 });
